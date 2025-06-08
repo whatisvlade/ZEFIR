b0d4d6b04f1f0706cc5116c77feec59e8fc6f6d2
whatisvlade <vladislavzyablov1997@gmail.com>
2025-06-08 23:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,6 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
