511102e9b21cdbf9b23711c6c9a642a44451cd93
whatisvlade <vladislavzyablov1997@gmail.com>
2025-06-09 15:35:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -104,4 +104,7 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    asyncio.get_event_loop().run_until_complete(main())
